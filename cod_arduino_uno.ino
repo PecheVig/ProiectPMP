25ef285a7f7ede999e0eb3d0465038dda59422c1
Vorniceanu Iuliana <ivorniceanu@yahoo.com>
2020-12-17 19:24:40 +0200

    Saptamana 12

--- cod_arduino_uno.ino
+++ cod_arduino_uno.ino
@@ -1,37 +1,122 @@
-/*
-  Blink
+#define mpin00 8
+#define mpin01 9
+
+#define mpin10 10
+#define mpin11 11
+
+
+#define echo_pin 2
+#define trig_pin 3 
+long duration; 
+int distance;
+
+
+char ch;
+char ch1;
+void setup()
+{
+ digitalWrite(mpin00, 0);
+ digitalWrite(mpin01, 0);
+ digitalWrite(mpin10, 0);
+ digitalWrite(mpin11, 0);
+ pinMode (mpin00, OUTPUT);
+ pinMode (mpin01, OUTPUT);
+ pinMode (mpin10, OUTPUT);
+ pinMode (mpin11, OUTPUT);
+
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+
  
-  Turns an LED on for one second, then off for one second, repeatedly.
- 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
- 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
- 
-  This example code is in the public domain.
- 
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
- 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+ Serial.begin(9600);
+  pinMode(13, OUTPUT);
 }
+void senzor(){
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+  duration = pulseIn(echo_pin, HIGH);
+  distance = duration * 0.034 / 2;
+  
+}
+void StartMotor (int m1, int m2, int forward, int speed)
+{
+
+ if (speed==0) // oprire
+ {
+ digitalWrite(m1, 0);
+ digitalWrite(m2, 0);
+ }
+ else
+ {
+ if (forward)
+ {
+ digitalWrite(m2, 0);
+   
+   analogWrite(m1, speed); // folosire PWM
+ }
+ else
+ {
+ digitalWrite(m1, 0);
+ analogWrite(m2, speed);
+ }
+ }
+}
+//aici ar trebuii sa schimb ca ch sa nu vina de la serial input ci de la modulul bluetooth master
+void delayStopped(int ms)
+{
+ StartMotor (mpin00, mpin01, 0, 0);
+ StartMotor (mpin10, mpin11, 0, 0);
+ delay(ms);
+}
+void serialEvent() {
+  ch=(char)Serial.read();
+}
+void loop()
+{
+  if ((ch=='W'||ch=='w')&&distance>20){
+    //Inainte
+    if (ch!=ch1){
+    delayStopped(500);
+      ch1=ch;
+    }
+  StartMotor (mpin00, mpin01, 1, 128);
+  StartMotor (mpin10, mpin11, 1, 128);
  
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  }
+  if ((ch=='s'||ch=='S')&&distance>20){
+    //Inapoi
+  StartMotor (mpin01, mpin00, 1, 128);
+  StartMotor (mpin11, mpin10, 1, 128);
+    ch='\n';
+  }
+  if ((ch=='a'||ch=='A')&&distance>20){
+    //Stanga
+     if (ch!=ch1){
+    delayStopped(500);
+      ch1=ch;
+    }
+  StartMotor (mpin01, mpin00, 1, 128);
+  StartMotor (mpin10, mpin11, 1, 128);
+  }
+  if ((ch=='d'||ch=='D')&&distance>20){
+    //Dreapta
+    if (ch!=ch1){
+    delayStopped(500);
+      ch1=ch;
+    }
+  StartMotor (mpin00, mpin01, 1, 128);
+  StartMotor (mpin11, mpin10, 1, 128);
+    ch='\n';
+  }
+  if (ch=='k'||ch=='K'||distance<=20){
+    //Stop
+  StartMotor (mpin00, mpin01, 1, 0);
+  StartMotor (mpin11, mpin10, 1, 0);
+    ch='\n';
+    delayStopped(500);
+  }
+ delay (500);
 }
