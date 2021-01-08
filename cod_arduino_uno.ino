cf304287a2442376f4eb0da8b2b94a96e0d3eae2
Vorniceanu Iuliana <ivorniceanu@yahoo.com>
2021-01-08 04:17:44 +0200

    Fourth commit

--- cod_arduino_uno.ino
+++ cod_arduino_uno.ino
@@ -1,20 +1,25 @@
-#define mpin00 8
-#define mpin01 9
-
-#define mpin10 10
+#include <Wire.h>
+#include <NewPing.h> 
+// Pinii motor 1
+#define mpin00 6
+#define mpin01 5
+// Pinii motor 2
+#define mpin10 3
 #define mpin11 11
+#define MAX_DISTANCE 250
 
-
-#define echo_pin 2
-#define trig_pin 3 
-long duration; 
-int distance;
-
+int distance = 100;
 
 char ch;
 char ch1;
-void setup()
-{
+
+#define echo_pin A2
+#define trig_pin A1
+#define max_dist 200
+
+NewPing sonar(trig_pin, echo_pin, max_dist);
+void setup() {
+ // configurarea pinilor motor ca iesire, initial valoare 0
  digitalWrite(mpin00, 0);
  digitalWrite(mpin01, 0);
  digitalWrite(mpin10, 0);
@@ -23,24 +28,21 @@ void setup()
  pinMode (mpin01, OUTPUT);
  pinMode (mpin10, OUTPUT);
  pinMode (mpin11, OUTPUT);
-
- pinMode(trigPin, OUTPUT);
- pinMode(echoPin, INPUT);
-
  
  Serial.begin(9600);
-  pinMode(13, OUTPUT);
+ // pin LED
+ pinMode(13, OUTPUT);
+ delay(2000);
+ distance = readPing();    // Get Ping Distance.
+ delay(100);
+ 
 }
 void senzor(){
-  digitalWrite(trig_pin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig_pin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig_pin, LOW);
-  duration = pulseIn(echo_pin, HIGH);
-  distance = duration * 0.034 / 2;
-  
+  int distance = readPing();
+  delay(100);
 }
+// Funcție pentru controlul unui motor
+// Intrare: pinii m1 și m2, direcția și viteza
 void StartMotor (int m1, int m2, int forward, int speed)
 {
 
@@ -54,8 +56,7 @@ void StartMotor (int m1, int m2, int forward, int speed)
  if (forward)
  {
  digitalWrite(m2, 0);
-   
-   analogWrite(m1, speed); // folosire PWM
+ analogWrite(m1, speed); // folosire PWM
  }
  else
  {
@@ -64,20 +65,26 @@ void StartMotor (int m1, int m2, int forward, int speed)
  }
  }
 }
-//aici ar trebuii sa schimb ca ch sa nu vina de la serial input ci de la modulul bluetooth master
+// Funcție de siguranță
+// Execută oprire motoare, urmată de delay
 void delayStopped(int ms)
 {
  StartMotor (mpin00, mpin01, 0, 0);
  StartMotor (mpin10, mpin11, 0, 0);
  delay(ms);
 }
+
 void serialEvent() {
   ch=(char)Serial.read();
 }
 void loop()
 {
+  distance=readPing();
+  Serial.println(distance);
+  
   if ((ch=='W'||ch=='w')&&distance>20){
     //Inainte
+    Serial.println(ch);
     if (ch!=ch1){
     delayStopped(500);
       ch1=ch;
@@ -85,8 +92,7 @@ void loop()
   StartMotor (mpin00, mpin01, 1, 128);
   StartMotor (mpin10, mpin11, 1, 128);
  
-  }
-  if ((ch=='s'||ch=='S')&&distance>20){
+  }else if ((ch=='s'||ch=='S')&&distance>20){
     //Inapoi
   StartMotor (mpin01, mpin00, 1, 128);
   StartMotor (mpin11, mpin10, 1, 128);
@@ -100,8 +106,7 @@ void loop()
     }
   StartMotor (mpin01, mpin00, 1, 128);
   StartMotor (mpin10, mpin11, 1, 128);
-  }
-  if ((ch=='d'||ch=='D')&&distance>20){
+  }else if ((ch=='d'||ch=='D')&&distance>20){
     //Dreapta
     if (ch!=ch1){
     delayStopped(500);
@@ -110,8 +115,7 @@ void loop()
   StartMotor (mpin00, mpin01, 1, 128);
   StartMotor (mpin11, mpin10, 1, 128);
     ch='\n';
-  }
-  if (ch=='k'||ch=='K'||distance<=20){
+  }else if (ch=='k'||ch=='K'||distance<=20){
     //Stop
   StartMotor (mpin00, mpin01, 1, 0);
   StartMotor (mpin11, mpin10, 1, 0);
@@ -119,4 +123,15 @@ void loop()
     delayStopped(500);
   }
  delay (500);
+}
+
+int readPing()      // Read Ping Function for Ultrasonic Sensor.
+{
+  delay(100);                 // Wait 100ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  int cm = sonar.ping_cm();   //Send ping, get ping distance in centimeters (cm).
+  if (cm==0)
+  {
+    cm=250;
+  }
+  return cm;
 }
