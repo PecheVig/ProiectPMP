db594f41a429d1b5667c1c12db87555dc4d90eeb
Vorniceanu Iuliana <ivorniceanu@yahoo.com>
2021-01-10 14:14:10 +0200

    Sixth commit

--- cod_arduino_nano.ino
+++ cod_arduino_nano.ino
@@ -2,7 +2,6 @@
 #include "Wire.h" // This library allows you to communicate with I2C devices.
 const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
 int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
-char tmp_str[7]; // temporary variable used in convert function
 char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
   sprintf(tmp_str, "%6d", i);
   return tmp_str;
