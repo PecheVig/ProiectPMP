73977efd16015716d2202b8342cb516af11bb42d
Vorniceanu Iuliana <ivorniceanu@yahoo.com>
2021-01-10 03:10:26 +0200

    Fifth commit

--- cod_arduino_nano.ino
+++ cod_arduino_nano.ino
@@ -1,45 +1,50 @@
-#define xInp A1
-#define yInp A2
-#define zInp A3
-
-int RawMin = 0;
-int RawMax = 1023;
-
-const int sampleSize = 10;
-
-void setup() {
-  analogReference(EXTERNAL);
-  Serial.begin(9600);
-
-  
+// (c) Michael Schoeffler 2017, http://www.mschoeffler.de
+#include "Wire.h" // This library allows you to communicate with I2C devices.
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+char tmp_str[7]; // temporary variable used in convert function
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
 }
-
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+}
 void loop() {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 6, true); // request a total of 3*2=6 registers
   
-  int x=ReadAxis(xInp);
-  int y=ReadAxis(yInp);
-  int z=ReadAxis(zInp);
-
-
-  long xScaled = map(xRaw, RawMin, RawMax, -3000, 3000);
-  long yScaled = map(yRaw, RawMin, RawMax, -3000, 3000);
-  long zScaled = map(zRaw, RawMin, RawMax, -3000, 3000);
-
-  float xAccel = xScaled / 1000.0;
-  float yAccel = yScaled / 1000.0;
-  float zAccel = zScaled / 1000.0;
-  //cazuri pentru fiecare directie in parte....stanga, dreapta, inainte, inapoi
-  if(xAccel==-1&&yAccel==-0&&zAccel==-0){
-    //transmit semnalul de inapoi spre modulul bluetooth slave
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  //citim valorile pentru componentele x, y si z ale accelerometrului.
+  accelerometer_x = ( Wire.read()| Wire.read() << 8);
+  accelerometer_x = accelerometer_x/256;
+  accelerometer_y = ( Wire.read()| Wire.read() << 8); 
+  accelerometer_y=accelerometer_y/256;
+  accelerometer_z = ( Wire.read()| Wire.read() << 8); 
+  accelerometer_z=accelerometer_z/256;
+  //verificam pe care pozitie suntem
+  if(accelerometer_x<1000&&accelerometer_y<-8000){
+  //stanga
+  Serial.println('L');
   }
-  if(xAccel==-0&&yAccel==-1&&zAccel==-0){
-    //transmit semnalul de dreapta spre modulul bluetooth slave
+  if(accelerometer_x<1000&&accelerometer_y>8000){
+  //drapta
+  Serial.println('R');
   }
-  if(xAccel==-0&&yAccel==1&&zAccel==-0){
-    //transmit semnalul de stanga spre modulul bluetooth slave
+  if(accelerometer_x>8000&&accelerometer_y<1000){
+  //drapta
+  Serial.println('G'); 
   }
-  if(xAccel==1&&yAccel==-0&&zAccel==-0){
-    //transmit semnalul de inainte spre modulul bluetooth slave
+  if(accelerometer_x<-8000&&accelerometer_y<1000){
+    //stanga
+    Serial.println('F');
   }
-  
+  delay(1000);
 }
--- cod_arduino_uno.ino
+++ cod_arduino_uno.ino
@@ -1,4 +1,3 @@
-#include <Wire.h>
 #include <NewPing.h> 
 // Pinii motor 1
 #define mpin00 6
@@ -7,17 +6,20 @@
 #define mpin10 3
 #define mpin11 11
 #define MAX_DISTANCE 250
-
+//declararea unei variabile distance care este dolosita pentru a calcula distanta dintre un obiect aflat in fata masinutei si masinuta
 int distance = 100;
-
+//declararea unei variabile care va fi folosita pentru citirea caracterelor trimise prin bluetooth
 char ch;
-char ch1;
-
+//definirea pinilor pentru senzorul ultrasonic
 #define echo_pin A2
 #define trig_pin A1
+
+//definirea unei distante maxime pana la care sa caute sonarul
 #define max_dist 200
 
+//declararea unui obiect de tip NewPing pentru a lucra mult mai usor cu acesta
 NewPing sonar(trig_pin, echo_pin, max_dist);
+
 void setup() {
  // configurarea pinilor motor ca iesire, initial valoare 0
  digitalWrite(mpin00, 0);
@@ -29,17 +31,11 @@ void setup() {
  pinMode (mpin10, OUTPUT);
  pinMode (mpin11, OUTPUT);
  
- Serial.begin(9600);
- // pin LED
- pinMode(13, OUTPUT);
+ Serial.begin(38400);
  delay(2000);
- distance = readPing();    // Get Ping Distance.
+ distance = readPing();    //citirea distantei initiale
  delay(100);
  
-}
-void senzor(){
-  int distance = readPing();
-  delay(100);
 }
 // Funcție pentru controlul unui motor
 // Intrare: pinii m1 și m2, direcția și viteza
@@ -73,65 +69,56 @@ void delayStopped(int ms)
  StartMotor (mpin10, mpin11, 0, 0);
  delay(ms);
 }
-
-void serialEvent() {
-  ch=(char)Serial.read();
-}
 void loop()
 {
-  distance=readPing();
-  Serial.println(distance);
-  
-  if ((ch=='W'||ch=='w')&&distance>20){
-    //Inainte
-    Serial.println(ch);
-    if (ch!=ch1){
-    delayStopped(500);
-      ch1=ch;
-    }
-  StartMotor (mpin00, mpin01, 1, 128);
-  StartMotor (mpin10, mpin11, 1, 128);
- 
-  }else if ((ch=='s'||ch=='S')&&distance>20){
-    //Inapoi
-  StartMotor (mpin01, mpin00, 1, 128);
-  StartMotor (mpin11, mpin10, 1, 128);
-    ch='\n';
+  //verificam daca prin bluetooth se transmite ceva si daca da atunci punem acel ceva in variabila ch si afisam la serial monitor ce am citit
+  if(Serial.available()){
+  ch = Serial.read();
+  Serial.println(ch);
   }
-  if ((ch=='a'||ch=='A')&&distance>20){
+  //verificam pentru fiecare din inputul primit in ce caz suntem 
+  if (ch=='F'){
+    //Inainte
+    distance=readPing();
+    //verificam ca in fata masinutei sa nu se afle niciun obstacol, altfel daca se afla masinuta nu inainteaza
+    if(distance>20){
+  StartMotor (mpin00, mpin01, 1, 150);
+  StartMotor (mpin10, mpin11, 1, 150);
+  }
+  }else if (ch=='G'){
+    //Inapoi
+    distance=readPing();
+    
+  StartMotor (mpin00, mpin01, 0, 150);
+  StartMotor (mpin10, mpin11, 0, 150);
+  }else if (ch=='L'){
     //Stanga
-     if (ch!=ch1){
-    delayStopped(500);
-      ch1=ch;
-    }
-  StartMotor (mpin01, mpin00, 1, 128);
+    distance=readPing();
+    if(distance>20){
   StartMotor (mpin10, mpin11, 1, 128);
-  }else if ((ch=='d'||ch=='D')&&distance>20){
+  }
+  }else if (ch=='R'){
     //Dreapta
-    if (ch!=ch1){
-    delayStopped(500);
-      ch1=ch;
-    }
+    distance=readPing();
+    if(distance>20){
   StartMotor (mpin00, mpin01, 1, 128);
-  StartMotor (mpin11, mpin10, 1, 128);
-    ch='\n';
-  }else if (ch=='k'||ch=='K'||distance<=20){
+  }
+  }else if (ch=='S'){
     //Stop
   StartMotor (mpin00, mpin01, 1, 0);
   StartMotor (mpin11, mpin10, 1, 0);
-    ch='\n';
     delayStopped(500);
   }
  delay (500);
 }
 
-int readPing()      // Read Ping Function for Ultrasonic Sensor.
+int readPing()      //functie pentru determinarea distantei
 {
-  delay(100);                 // Wait 100ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-  int cm = sonar.ping_cm();   //Send ping, get ping distance in centimeters (cm).
+  delay(100);
+  int cm = sonar.ping_cm();   //functia ping_cm returneaza o valoare incepand de la 1 pana la distanta maxima daca exista un obiect in radar sau 0 daca nu se afla nimic in fata masinutei
   if (cm==0)
   {
-    cm=250;
+    cm=200; //daca nu se afla nimic in fata masinutei atunci setam varianila cm la valoarea maxima 
   }
   return cm;
 }
